a09ba76a953865c6d9ce3262fe758adfab0ab847
r0ma <wbfx@ya.ru>
2016-10-02 17:12:17 +0500

    работает на "нормальных" городах

--- single_town.php
+++ single_town.php
@@ -3,17 +3,23 @@ set_time_limit(0);
 include 'simple_html_dom.php';
 
 // $goroda = ['Абакан', 'Абинск'];
-$goroda = file( 'output.txt' );
+$goroda = file( '1.txt' );
 
 foreach ($goroda as $gorod) {
-    $html = file_get_html("https://ru.wikipedia.org/wiki/" . $gorod);
-    $arr = $html->find('table tr td', 0);
-    if ($arr !== '') {
+    $url = "https://ru.wikipedia.org/wiki/" . $gorod;
+    $headers = get_headers($url, 1);
+    if ( $headers[1] == 'HTTP/1.1 404 Not Found') {
+        echo $gorod . " - HTTP/1.1 404 Not Found</br>";
+    } else {
+    $html = file_get_html($url);
+    if($html && is_object($html)) {
+        $arr = $html->find('table tr td', 0);
         $a = $arr->plaintext;
         $html->clear();
         unset($html);
         $str_start = explode('Население', $a );
         $str_end = @explode('человек (2', $str_start[1]);
-        echo $gorod . " " . $str_end[0] . "<br>";
-    } else echo "страница для города $gorod не найдена";
+        echo $gorod . " " . $str_end[0] . "</br>";
+    }
+    }
 }
