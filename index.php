b7d5f8985de4e0f9e5c0b74a52f0690fcc45ad28
r0ma <wbfx@ya.ru>
2016-11-06 02:39:24 +0500

    тестирую предварительную очистку файлов от "мусора"

--- index.php
+++ index.php
@@ -18,22 +18,83 @@
 
 <p>
     добавлено городов: <?php echo $countAddsOffice; ?> из <?php echo $countGoroda; ?>
+
+<!-- <select name="added-city">
+    <option name="" value=""> - - - - -  </option> -->
+    <?php
+        require 'connect.php';
+        $stmts = $pdo->query("SELECT DISTINCT `gorod_id` FROM `office`");
+        while ($rows = $stmts->fetch()) {
+            $id_goroda = $rows['gorod_id'];
+            $stmts2 = $pdo->query("SELECT `gorod` FROM `goroda` WHERE `id` = $id_goroda");
+            while ($stroka = $stmts2->fetch()) {
+                $added_city[] = $stroka['gorod'];
+            }
+        }
+        $pdo = NULL;
+    ?>
 </p>
+
+<?php foreach ($added_city as $city) {
+    echo $city . "<br>";
+} ?>
 
 <p>
     <input type="submit" name="submit" value="Добавить">
 </p>
 
+<p>
+    <input type="submit" name="clean_atms_file" value="Очистить файлы от 'шлака'">
+</p>
+
 </form>
 
 <?php
+if ( !empty($_POST['clean_atms_file']) ) {
+
+    $office = file('office.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($office as $off) {
+        if ( $off == 'Оценить' ) {
+            $off = '';
+        }
+
+        // if ($off == '(сотрудник Почта Банка)') {
+        //     $off = '';
+        // }
+        //
+        // if ( $off == 'Клиентский центр в отделении Почты России' ) {
+        //     $off .= ' (сотрудник Почта Банка)';
+        // }
+        
+        $office_array[] = $off . "\n";
+        file_put_contents('office.txt', $office_array);
+    }
+    echo "<pre>";
+    print_r( $office_array );
+    echo "</pre>";
+
+    // $atms = file('atms.txt', FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+    // foreach ($atms as $atm) {
+    //     if ( $atm == 'Валюта: рубли') {
+    //         $atm = '';
+    //     }
+    //     $atm = str_replace('Услуги: ', '', $atm);
+    //     $arm_array[] = $atm . "\n";
+    //     file_put_contents('atms.txt', $arm_array);
+    // }
+
+}
+
+
+
 if ( !empty($_POST['submit']) ) {
     $gorod_id = $_POST['gorod'];
     if ($gorod_id == '0') {
         echo "Не указан город";
         exit();
     }
-    
+
     require 'connect.php';
 
     $stmt = $pdo->prepare("INSERT INTO `office` (gorod_id, office_feature_id, adress, comment, location, mode)
